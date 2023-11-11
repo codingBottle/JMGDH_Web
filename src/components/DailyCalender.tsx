835b35b6069c70455a9b07f4d4dec69782d9d24d
LeeHueeng <zzxx373014@gmail.com>
2023-11-12 03:44:06 +0900

    feat #3 - 화살표 SVG로 assets/icon에 추가

--- src/components/DailyCalender.tsx
+++ src/components/DailyCalender.tsx
@@ -1,5 +1,17 @@
 import React from "react";
-
+import styled from "styled-components";
+import { RightArrow, LeftArrow } from "@/assets/icon/Arrow";
 export default function DailyCalender() {
-  return <div>dailycalender</div>;
+  return (
+    <Container>
+      <RightArrow />
+      <LeftArrow />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: yellow;
+`;
