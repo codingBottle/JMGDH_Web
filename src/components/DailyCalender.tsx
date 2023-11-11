109040f9e24d802965a7a4d4ae307684a2008a1b
LeeHueeng <zzxx373014@gmail.com>
2023-11-12 04:07:34 +0900

    feat #2 - figma daily 상단 부분 틀 잡기

--- src/components/DailyCalender.tsx
+++ src/components/DailyCalender.tsx
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { RightArrow, LeftArrow } from "@/assets/icon/Arrow";
 export default function DailyCalender() {
+  const [date, setDate] = useState(new Date());
+  const [day, setDay] = useState(date.getDay());
+  const [month, setMonth] = useState(date.getMonth());
+  const [year, setYear] = useState(date.getFullYear());
   return (
-    <Container>
-      <RightArrow />
-      <LeftArrow />
-    </Container>
+    <DailyCalenderContenter>
+      <Daily>
+        <div className="Top">
+          <RightArrow />
+          <p className="daily">Daily</p>
+          <LeftArrow />
+        </div>
+        <div className="Bottom"></div>
+      </Daily>
+    </DailyCalenderContenter>
   );
 }
 
-const Container = styled.div`
+const DailyCalenderContenter = styled.div`
   width: 100%;
   height: 100%;
   background-color: yellow;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+`;
+
+const Daily = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 10%;
+  background-color: red;
+
+  .Top {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+  .daily {
+    margin: 0 20px;
+    font-size: 18px;
+  }
 `;
