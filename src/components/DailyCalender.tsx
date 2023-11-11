e9c49c26bdd59b4360062000a2b4beabd463b7af
LeeHueeng <zzxx373014@gmail.com>
2023-11-12 03:44:22 +0900

    Revert "feat #3 - 화살표 SVG로 assets/icon에 추가"
    This reverts commit 835b35b6069c70455a9b07f4d4dec69782d9d24d.

--- src/components/DailyCalender.tsx
+++ src/components/DailyCalender.tsx
@@ -1,17 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-import { RightArrow, LeftArrow } from "@/assets/icon/Arrow";
-export default function DailyCalender() {
-  return (
-    <Container>
-      <RightArrow />
-      <LeftArrow />
-    </Container>
-  );
-}
 
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: yellow;
-`;
+export default function DailyCalender() {
+  return <div>dailycalender</div>;
+}
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 export default function hello() {
   return (
     <Container>
+      sdfsddsafs
       <DailyCalender />
     </Container>
   );
