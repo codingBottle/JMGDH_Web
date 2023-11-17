b6544e0b9e85ecdc9e05ff4617c33382c4c9637e
LeeHueeng <zzxx373014@gmail.com>
2023-11-17 09:35:31 +0900

    feat #2 - figma daily 하단부분(날짜부분 구상 잡기)

--- src/components/DailyCalender.tsx
+++ src/components/DailyCalender.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { RightArrow, LeftArrow } from "@/assets/icon/Arrow";
+import { Content } from "next/font/google";
+
 export default function DailyCalender() {
   const [date, setDate] = useState(new Date());
   const [day, setDay] = useState(date.getDay());
   const [month, setMonth] = useState(date.getMonth());
   const [year, setYear] = useState(date.getFullYear());
+
+  const numbers = Array.from({ length: 24 }, (_, index) => index);
+
   return (
     <DailyCalenderContenter>
       <Daily>
@@ -14,8 +19,25 @@ export default function DailyCalender() {
           <p className="daily">Daily</p>
           <LeftArrow />
         </div>
-        <div className="Bottom"></div>
+        <div className="Bottom">
+          <p>{day}</p>
+        </div>
       </Daily>
+      <Contents>
+        <table>
+          <tbody>
+            {[...Array(numbers)].map((i) => (
+              <tr key={i.toString()}>
+                {numbers.map((num) => (
+                  <td className="Number" key={num}>
+                    {num}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Contents>
     </DailyCalenderContenter>
   );
 }
@@ -23,29 +45,45 @@ export default function DailyCalender() {
 const DailyCalenderContenter = styled.div`
   width: 100%;
   height: 100%;
-  background-color: yellow;
+
   border: 1px solid black;
-  display: flex;
-  justify-content: center;
 `;
 
 const Daily = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   width: 100%;
-  height: 10%;
-  background-color: red;
-
+  height: 15%;
   .Top {
     display: flex;
     justify-content: center;
     align-items: center;
+    margin-top: 30px;
     width: 100%;
-    height: 100%;
   }
   .daily {
     margin: 0 20px;
     font-size: 18px;
+  }
+  .Bottom {
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    p {
+      font-size: 10px;
+    }
+  }
+`;
+const Contents = styled.div`
+  width: 100%;
+  height: 80%;
+  margin-top: 10px;
+  overflow: auto;
+  table {
+    margin-top: 10px;
+    width: 185%;
+  }
+  .Number {
+    width: 40px;
   }
 `;
