2b2b393ec70eeca37db621ac347cf839d3d01374
LeeHueeng <zzxx373014@gmail.com>
2023-11-24 14:32:03 +0900

    feat #9 - 속성값 추가

--- src/components/CalenderNavbar.tsx
+++ src/components/CalenderNavbar.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import theme from "@/styles/theme/theme";
 
 export default function CalenderNavbar() {
-  return <CalenderNavbarCss>CalenddfsferNavbar</CalenderNavbarCss>;
+  return (
+    <CalenderNavbarCss>
+      <div>
+        <button>TODAY</button>
+        <button>&lt;</button>
+        <button>&gt;</button>
+      </div>
+      <div>
+        <button>M</button>
+        <button>Y</button>
+        <button>N</button>
+      </div>
+    </CalenderNavbarCss>
+  );
 }
 
 const CalenderNavbarCss = styled.div`
   width: 100%;
-  height: 80px;
-  background-color: black;
+  height: 100%;
+  padding: 14px 20px;
+  margin: 0 auto;
+  border: 1px solid ${theme.color.SecondaryColor.Border};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
