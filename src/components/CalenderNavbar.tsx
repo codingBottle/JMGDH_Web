1884eb996188c76ebcd8c17093f53e492e22ff73
LeeHueeng <zzxx373014@gmail.com>
2023-11-24 14:19:32 +0900

    feat #9 - 캘린더 navbar 컴포넌트 생성및 구도 잡기 나중에 수정예정

---
+++ src/components/CalenderNavbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function CalenderNavbar() {
+  return <CalenderNavbarCss>CalenddfsferNavbar</CalenderNavbarCss>;
+}
+
+const CalenderNavbarCss = styled.div`
+  width: 100%;
+  height: 80px;
+  background-color: black;
+`;
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,17 +2,50 @@ import React from "react";
 import DailyCalender from "@/components/DailyCalender";
 import styled from "styled-components";
 import Navbar from "@/components/Navbar";
+import CalenderNavbar from "@/components/CalenderNavbar";
 
 export default function hello() {
   return (
     <div>
       <Navbar />
-      <Container>
-        <DailyCalender />
-      </Container>
+      <MainContent>
+        <div className="Leftbox">
+          <Container>
+            <DailyCalender />
+          </Container>
+        </div>
+        <div className="Rightbox">
+          <div className="CalNavBar">
+            <CalenderNavbar />
+          </div>
+        </div>
+      </MainContent>
     </div>
   );
 }
+
+const MainContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  .Leftbox {
+    display: flex;
+    justify-content: left;
+    width: 266px;
+    height: 100%;
+  }
+  .Rightbox {
+    display: flex;
+    justify-content: left;
+    width: 100%;
+    height: 100%;
+    .CalNavBar {
+      width: 1576px;
+      height: 68px;
+    }
+  }
+`;
 
 const Container = styled.div`
   width: 266px;
