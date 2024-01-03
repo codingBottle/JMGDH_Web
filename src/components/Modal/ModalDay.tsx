1d45723ad04c762ee7185c549395aced9e25e260
“Dami-LEE00” <dxmxxii@gmail.com>
2024-01-03 22:40:58 +0900

    feat #12 - ModalDay 컴포넌트 생성

---
+++ src/components/Modal/ModalDay.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from "styled-components";
+import theme from "@/styles/theme/theme";
+
+const ModalDay = () => {
+  return (
+    <div>ModalDay</div>
+  )
+}
+
+export default ModalDay;
