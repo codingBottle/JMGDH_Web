01590fa5e2fdd3b975dd13665ab9b446eda71925
LeeHueeng <zzxx373014@gmail.com>
2024-02-16 18:54:21 +0900

    현우집 수정중

---
+++ src/pages/oauth2/[id].tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import styled, { keyframes } from "styled-components";
+import axios from "axios";
+
+export default function Hello() {
+  const router = useRouter();
+  const [oneTimeUseCode, setOneTimeUseCode] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+  const [accectoken, setAccessToken] = useState("");
+  useEffect(() => {
+    if (oneTimeUseCode !== null) {
+      console.log("나와짜샤 ");
+      axios
+        .get(
+          `https://calendars2.duckdns.org/oauth/token?code=${oneTimeUseCode}`
+        )
+        .then((response) => {
+          console.log("response");
+          setAccessToken(response.data.accessToken);
+          setLoading(false);
+
+          console.log(response?.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching access token:", error);
+          setLoading(false);
+        });
+    }
+  }, [oneTimeUseCode]);
+  useEffect(() => {
+    const code = router.query.oneTimeUseCode;
+
+    if (code) {
+      console.log("Received oneTimeUseCode:", code);
+      setOneTimeUseCode(code);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    }
+  }, [router.query.oneTimeUseCode]);
+
+  return (
+    <Wrapper>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <Content>
+          <LeftSection>{/* Left Section Content */}</LeftSection>
+          <MiddleSection>
+            <div className="calenderNav">{/* Calendar Navbar */}</div>
+            {/* Month Calendar */}
+          </MiddleSection>
+          <RightSection>{/* Right Section Content */}</RightSection>
+        </Content>
+      )}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: calc(100vh - 80px);
+`;
+
+const LeftSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 266px;
+  height: 100%;
+`;
+
+const MiddleSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
+  .calenderNav {
+    height: 68px;
+    background-color: #999;
+  }
+`;
+
+const RightSection = styled.div`
+  width: 78px;
+  height: 100%;
+`;
+
+const TodoContainer = styled.div`
+  width: 266px;
+  height: 598px;
+`;
