0bb799f2a4a7091c7ae2cba5cbd1c028d99b218a
“Dami-LEE00” <dxmxxii@gmail.com>
2024-02-20 20:55:04 +0900

    캘린더 모달창 css 임시완성

--- src/components/CalendarComponent.tsx
+++ src/components/CalendarComponent.tsx
@@ -16,31 +16,36 @@ const CalendarComponent = () => {
   const closeModal = () => {
     setShowModal(false);
   };
-  const handlePrevMonth = () => {
-    const newDate = new Date(selectedDate);
-    newDate.setMonth(newDate.getMonth() - 1);
-    setSelectedDate(newDate);
-  };
+  // const handlePrevMonth = () => {
+  //   const newDate = new Date(selectedDate);
+  //   newDate.setMonth(newDate.getMonth() - 1);
+  //   setSelectedDate(newDate);
+  // };
 
-  const handleNextMonth = () => {
-    const newDate = new Date(selectedDate);
-    newDate.setMonth(newDate.getMonth() + 1);
-    setSelectedDate(newDate);
-  };
+  // const handleNextMonth = () => {
+  //   const newDate = new Date(selectedDate);
+  //   newDate.setMonth(newDate.getMonth() + 1);
+  //   setSelectedDate(newDate);
+  // };
+
   return (
     <>
       <CalendarWrapper>
-        <RightArrow onClick={handleNextMonth} />
-        <LeftArrow onClick={handlePrevMonth} />
         <Calendar
           onChange={handleDateChange}
           value={selectedDate}
           calendarType="US"
+          locale="ko-KO" // 한글버전
           className="custom-calendar"
+          minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+          maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+          next2Label={null}
+          prev2Label={null}
           tileContent={({ date }) => {
             return date.getDate();
           }}
         />
+        <CompleteBtn>완료</CompleteBtn>
       </CalendarWrapper>
       {showModal && (
         <Modal>
@@ -64,16 +69,41 @@ const CalendarWrapper = styled.div`
   border-radius: 8px;
   padding: 16px;
   width: 254px;
-  height: 331px;
+  font-size: 12px;
+  text-align: center;
 
   .react-calendar__navigation__label__labelText,
   .react-calendar__navigation__label__labelText--from {
     flex-grow: 0.5;
   }
+
   .react-calendar__month-view__days {
     height: 228px;
     width: 218px;
   }
+
+  abbr {
+    display: inline-block;
+    font-size: 12px;
+    text-decoration: none;
+  }
+
+  /* 요일 */
+  .react-calendar__month-view__weekdays__weekday{
+    display: inline-block;
+    color: #000;
+    margin-bottom: 10px;
+    abbr {
+      border: none;
+    }
+  }
+
+  .react-calendar__month-view__weekdays {
+    display: inline-block;
+    color: #000 !important;
+    text-align: center;
+  }
+
   .react-calendar__tile,
   .react-calendar__month-view__days__day,
   .react-calendar__month-view__days__day--neighboringMonth {
@@ -82,25 +112,83 @@ const CalendarWrapper = styled.div`
     align-items: center;
   }
 
-  .react-calendar__navigation__label {
-    display: flex;
-    border: none;
-    flex-grow: 0.5;
+  /* 저번 달 & 다음 달 일자 */
+  .react-calendar__month-view__days__day--neighboringMonth{
+    color: ${theme.color.GrayScale.Gray300};
   }
-  .react-calendar__navigation {
-    display: flex;
-    margin-bottom: 10px;
-    width: 260px;
+
+  .react-calendar__tile {
+    border: none;
+    font-size: 12px;
+    width: 30px;
     height: 30px;
-    .react-calendar__navigation__arrow,
-    react-calendar__navigation__prev2-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &:hover {
+      background-color: ${theme.color.HoverEventColor.EventfillHover};
+      border-radius: 100px;
+    }
+    &:focus {
+      background-color: ${theme.color.HoverEventColor.EventfillHover};
+      border-radius: 100px;
+      border: 1px solid rgba(0, 0, 0, 0.6);
+    }
+
+    abbr {
       display: none;
     }
   }
-  abbr {
-    display: none;
-    font-size: 12px;
+
+  /* 오늘 날짜 */
+  .react-calendar__tile--now{
+    background-color: rgba(0, 0, 0, 0.2) !important;
+    color: ${theme.color.PrimaryColor.PrimaryWhite};
+    border-radius: 100px;
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.4) !important;
+    }
   }
+
+  /* 선택된 날짜의 배경색 변경 */
+  .react-calendar__tile--active {
+    background-color: ${theme.color.HoverEventColor.EventfillHover};
+    border-radius: 100px;
+  }
+
+  .react-calendar__navigation {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    margin-bottom: 10px;
+    .react-calendar__navigation__arrow {
+      display: inline-block;
+      padding: 0 4px;
+      border: none;
+
+    }
+    .react-calendar__navigation__prev-button {
+      order: 2;
+      display: inline-block;
+      margin-right: 8px;
+      font-size: 32px;
+    }
+    .react-calendar__navigation__next-button {
+      order: 3;
+      font-size: 32px;
+    }
+  }
+
+  .react-calendar__navigation__label {
+    display: flex;
+    justify-content: flex-start;
+    border: none;
+    flex-grow: 0.5;
+    color: ${theme.color.SecondaryColor.BasicFont};
+  }
+
   .custom-calendar .react-calendar {
     border: none;
     display: flex;
@@ -109,12 +197,9 @@ const CalendarWrapper = styled.div`
     align-items: center;
   }
 
-  .custom-calendar .react-calendar__tile {
-    border: none;
-    font-size: 1rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  .react-calendar__navigation__label__labelText,
+  .react-calendar__navigation__label__labelText--from {
+    flex-grow: 0;
   }
 `;
 
@@ -135,6 +220,15 @@ const ModalContent = styled.div`
   border-radius: 8px;
   padding: 20px;
   z-index: 1;
+`;
+
+const CompleteBtn = styled.button`
+  /* width: 100%; */
+  height: 27px;
+  padding: 5px 90px;
+  border-radius: 4px;
+  font-size: 12px;
+  font-weight: ${theme.fontWeight.Regular};
 `;
 
 export default CalendarComponent;
