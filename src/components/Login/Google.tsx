ebe1402b1605dd401efc637b1c75eb7f9bced568
LeeHueeng <zzxx373014@gmail.com>
2024-02-24 00:32:02 +0900

    구글 뷰포트 추가

---
+++ src/components/Login/Google.tsx
@@ -0,0 +1,27 @@
+// LoginButton.js
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import theme from "@/theme/theme";
+import { useRouter } from "next/router";
+import axios from "axios";
+import Image from "next/image";
+
+export default function Google() {
+    
+    return (
+        <Container>
+          <p>dd</p>
+        </Container>
+      );
+
+  };
+  const Container = styled.div`
+  display : flex;
+  justify-content: center;
+  align-items: center; 
+  width:370px;
+  height: 470px;
+  background-color: black;
+
+  
+`;
